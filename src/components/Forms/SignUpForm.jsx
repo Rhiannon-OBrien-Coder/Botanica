84f8ad2ebdb8063804438b96d9d6f513da406d2d
rhiannon-obrien-coder <rhiannonobrien@gmail.com>
2024-12-17 12:17:37 -0600

    somehow the import of userservice got deleted on the signup form component - and right before presentation

--- src/components/Forms/SignUpForm.jsx
+++ src/components/Forms/SignUpForm.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import * as userService from '../../services/userService'
 
 const SignUpForm = (props) => {
   const navigate = useNavigate();
