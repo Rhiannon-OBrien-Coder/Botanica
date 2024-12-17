04028b91934aa56376262492b54c79cd59cfe7b1
rhiannon-obrien-coder <rhiannonobrien@gmail.com>
2024-12-16 19:45:34 -0600

    store is broken

--- src/components/Store/Store.jsx
+++ src/components/Store/Store.jsx
@@ -1,8 +1,43 @@
-import React from "react";
-import { Link } from "react-router-dom"
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import * as plotOptionService from "../../services/plotOptionService";
+import * as seedService from "../../services/seedService"
 
-const Store = (props) => {
+const Store = () => {
+  const [plotOptionsList, setPlotOptionsList] = useState([])
+  const [seedList, setSeedList] = useState([])
+  
+  useEffect(() => {
+    const plotOptionIndex = async () => {
+      try {
+        const plotOptions = await plotOptionService.index();
+        if (plotOptions.error) {
+          throw new Error(plotOptions.error);
+        }
+        setPlotOptionsList(plotOptions);
+      } catch (error) {
+        console.log("Error fetching plotOptions:", error);
+      }
+    };
+    plotOptionIndex();
+    console.log(plotOptionsList)
+  }, [plotOption]);
 
+  useEffect(() => {
+    const seedIndex = async () => {
+      try {
+        const seeds = await seedService.index();
+        if (seeds.error) {
+          throw new Error(seeds.error);
+        }
+        setSeedList(seeds);
+      } catch (error) {
+        console.log("Error fetching seeds:", error);
+      }
+    };
+    seedIndex();
+  }, [seed]);
 
   return (
     <>
@@ -10,9 +45,26 @@ const Store = (props) => {
       {localStorage.token ? (
             <>
               <h2>Plot Options:</h2>
-              <div className="store-item"></div>
+              {plotOptionsList.map((plotOption) => (
+                <div key={plotOption.id} className="store-item">
+                  <h3>{plotOption.name}</h3>
+                  <h3>{plotOption.season}</h3>
+                  <p>{plotOption.description}</p>
+                  <h3>{plotOption.price}</h3>
+                  <button>Buy</button>
+                </div>
+              ))}
               <h2>Seeds:</h2>
-              <div className="store-item"></div>
+              {seedList.map((seed) => (
+                <div key={seed.id} className="store-item">
+                  <h3>{seed.name}</h3>
+                  <h3>{seed.season}</h3>
+                  <p>{seed.description}</p>
+                  <h3>{seed.growth_period}</h3>
+                  <h3>{seed.price}</h3>
+                  <button>Buy</button>
+                </div>
+              ))}
             </>
             ) : (
               <div>
